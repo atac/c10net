14fc1b4031b9336700f5625f5e783e77159fd873
Stephen Frees <stephenf93@gmail.com>
2026-02-11 16:56:03 -0600

    Modified parse_chapter10.py to work as a source task

--- src/ch10net/parse_chapter10.py
+++ src/ch10net/parse_chapter10.py
@@ -1,35 +1,40 @@
-from queue import *
+"""
+Read packets from a Chapter 10 file, apply optional filters, and pass them to a provided data sink function.
+"""
+
 from threading import Event
 from chapter10 import C10
 
-__all__ = ['parse_file', 'retrieve_packets', 'terminate_event']
+__all__ = ['parse_file', 'terminate']
 
-channel_types = {} 
-channel_ids = {}
+_channel_types = {} 
+_channel_ids = {}
 
-QUEUE_SIZE = 10000
-GET_SIZE = 5000
-q = Queue(maxsize=QUEUE_SIZE)
+terminate = Event()
 
-terminate_event = Event()
-
-
-def parse_file(args):
+def parse_file(
+        channel_ids_filter : list, 
+        channel_types_filter : list, 
+        infile : str, 
+        data_sink_func : function
+        ):
     """Main entry point for parsing a Chapter 10 file.
     
     :param args: The argparse.Namespace object containing CLI arguments.
     """
-    set_filter_parameters(args.channel_ids, args.channel_types)
-    read_packets(args.infile)
+    _set_filter_parameters(channel_ids_filter, channel_types_filter)
+    _read_packets(infile, data_sink_func)
 
 
-def set_filter_parameters(ids, types):
+def _set_filter_parameters(ids, types):
     """
     Set the filter parameters for channel IDs and types. These parameters will be used to determine which packets to process.
     
     :param ids: List of channel IDs to include in processing.
     :param types: List of channel types to include in processing.
     """
+    global channel_types, channel_ids
+
     if (type(channel_ids) == type(ids)):
         channel_ids = ids
 
@@ -37,45 +42,31 @@ def set_filter_parameters(ids, types):
         channel_types = types
     
     
-def read_packets(infile):
+def _read_packets(infile, sink):
+    global terminate
+    
     for packet in C10(infile):
         
-        if terminate_event.is_set():
+        if terminate.is_set():
             # perform any per-thread cleanup here if needed
             break
 
-        if (passes_filter(packet.channel_id, packet.data_type)):
-            try:
-                q.put(packet)
-            except Exception:
-                print("exception in  parse_chapter10.read_packets while putting packet in queue")
-                break
+        if (_passes_filter(packet.channel_id, packet.data_type)):
+            sink([packet])
         else:
             continue
 
 
-def passes_filter(id, type):
+def _passes_filter(id, type):
     """Returns False if id or type are not in their respective [non-empty] filter lists."""
-    if (len(channel_ids) > 0 and id not in channel_ids):
+    global _channel_types, _channel_ids
+
+    if (len(_channel_ids) > 0 and id not in _channel_ids):
         return False
     
-    if (len(channel_types) > 0 and type not in channel_types):
+    if (len(_channel_types) > 0 and type not in _channel_types):
         return False
     
     return True
 
 
-def retrieve_packets():
-    """Generator that returns packets from the internal queue."""
-    packets = []
-    
-    if (not terminate_event.is_set()):
-            for i in range(GET_SIZE):
-                try:
-                    packets.append(q.get_nowait())
-                except Empty:
-                    break
-                except Exception:
-                    break
-    
-    return packets
