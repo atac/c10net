a026c67fff9d7b7b1b3666d03eabba00f1714fcc
Stephen Frees <stephenf93@gmail.com>
2026-02-11 16:53:31 -0600

    Make udp transfer header generator a self-contained object

--- src/ch10net/transfer_header_generator.py
+++ src/ch10net/transfer_header_generator.py
@@ -11,86 +11,85 @@ State is maintained to ensure correct sequencing of segmented packets.
 import struct
 
 
-__all__ = ['generate_transfer_header', 'reset']
+#__all__ = ['generate_transfer_header', 'reset']
 
-_seq_count = {}
-_udp_sequence = 0x0
+class UdpTransferHeaderGenerator():
+    def __init__(self):
+        self._seq_count = {}
+        self._udp_sequence = 0x0
+        self.MAX_BYTES_PER_MESSAGE = 32726
+        self.MAX_MSG_SIZE = 1472
 
-MAX_BYTES_PER_MESSAGE = 32726
-MAX_MSG_SIZE = 1472
+    def generate_transfer_headers(self, channel_id: int, packet_size: int):
+        """
+        Generate UDP transfer headers for a given channel ID.
 
-def generate_transfer_headers(channel_id: int, packet_size: int):
-    """
-    Generate UDP transfer headers for a given channel ID.
+        Returns a list of tuples containing (offset, header) for each segment.
+        """
+        headers = []
 
-    Returns a list of tuples containing (offset, header) for each segment.
-    """
-    headers = []
+        if (length > self.MAX_MSG_SIZE):
+            offset = 0
+            seq_num = self.GetChannelSequenceNum(channel_id)
 
-    if (length > MAX_MSG_SIZE):
-        offset = 0
-        seq_num = GetChannelSequenceNum(channel_id)
+            while (length > 0):
+                h = self.GetSegmentedUdpTransferHeader(channel_id, seq_num, offset)
+                headers.append((offset, h))
 
-        while (length > 0):
-            h = GetSegmentedUdpTransferHeader(channel_id, seq_num, offset)
-            headers.append((offset, h))
+                if (length > self.MAX_MSG_SIZE):
+                    offset += self.MAX_MSG_SIZE
+                    length -= self.MAX_MSG_SIZE
+                else:
+                    length = 0
+        else:
+            h = self.GetNonSegmentedUdpTransferHeader(channel_id)
+            headers.append((0, h))
 
-            if (length > MAX_MSG_SIZE):
-                offset += MAX_MSG_SIZE
-                length -= MAX_MSG_SIZE
-            else:
-                length = 0
-    else:
-        h = GetNonSegmentedUdpTransferHeader(channel_id)
-        headers.append((0, h))
+        return headers
 
-    return headers
+    def reset(self):
+        self._seq_count.clear()
+        self._udp_sequence = 0x0
 
-def reset():
-    _seq_count.clear()
-    global _udp_sequence
-    _udp_sequence = 0x0
+    def GetChannelSequenceNum(self, channel: int):
+        num = self._seq_count.get(channel)
 
-def GetChannelSequenceNum(channel: int):
-    num = _seq_count.get(channel)
-
-    if (num == None):
-        num = 0
-    else:
-        if (num == 0xFF):
+        if (num == None):
             num = 0
         else:
-            num += 1
+            if (num == 0xFF):
+                num = 0
+            else:
+                num += 1
 
-    _seq_count[channel] = num
+        self._seq_count[channel] = num
 
-    return num
+        return num
 
-def GetUdpSequenceNum():
-    global udp_sequence
-    if (udp_sequence == 0xFFFFFF):
-        udp_sequence = 0
-    else:
-        udp_sequence += 1
-    
-    return udp_sequence
+    def GetUdpSequenceNum(self):
+        if (self.udp_sequence == 0xFFFFFF):
+            self.udp_sequence = 0
+        else:
+            self.udp_sequence += 1
+        
+        return self.udp_sequence
 
-def GetUdpTransferHeaderFirstWord(channel: int, segmented: bool):
-    ver = 0b0001
-    type = 0b0001 if segmented else 0b0000
-    seq_num = GetUdpSequenceNum()
-    word = (seq_num << 8) | (type << 4) | ver
-    header = struct.pack("<I", word)
-    return header
+    def GetUdpTransferHeaderFirstWord(self, channel: int, segmented: bool):
+        ver = 0b0001
+        type = 0b0001 if segmented else 0b0000
+        seq_num = self.GetUdpSequenceNum()
+        word = (seq_num << 8) | (type << 4) | ver
+        header = struct.pack("<I", word)
+        return header
 
-def GetNonSegmentedUdpTransferHeader(channel: int):
-    return GetUdpTransferHeaderFirstWord(channel, False)
+    def GetNonSegmentedUdpTransferHeader(self, channel: int):
+        return self.GetUdpTransferHeaderFirstWord(channel, False)
 
-def GetSegmentedUdpTransferHeader(channel: int, sequence_num: int, offset: int):
-    headerWord1 = GetUdpTransferHeaderFirstWord(channel, True)
+    def GetSegmentedUdpTransferHeader(self, channel: int, sequence_num: int, offset: int):
+        headerWord1 = self.GetUdpTransferHeaderFirstWord(channel, True)
 
-    word = (sequence_num << 16) | (channel & 0xFF)
-    headerWord2 = struct.pack("<I", word)
-    headerWord3 = struct.pack("<I", offset)
+        word = (sequence_num << 16) | (channel & 0xFF)
+        headerWord2 = struct.pack("<I", word)
+        headerWord3 = struct.pack("<I", offset)
 
-    return headerWord1 + headerWord2 + headerWord3
+        return headerWord1 + headerWord2 + headerWord3
