bc8efd5f683448102874b368146888f8446164e5
Stephen Frees <stephenf93@gmail.com>
2026-02-19 11:53:16 -0600

    Integrated setup packet pulse in replay task

--- src/c10net/chapter10_to_replay.py
+++ src/c10net/chapter10_to_replay.py
@@ -1,22 +1,30 @@
 
 import os
 
-from chapter10 import C10
+from chapter10 import C10, packet
 
 import tasks.parse_chapter10 as pc10
 import tasks.udp_replay as replay
 
 import functions.ethernet_packet_generator as ethernet_gen
 import functions.progress_bar as bar
+import functions.setup_packet_pulse as pulse
 
 
 
 def run_task(cli_args):
     eth_gen = ethernet_gen.EthernetGenerator(cli_args)
 
+    pulser = None
+    use_pulse = False
+    if hasattr(cli_args, 'pulse_interval'):
+        use_pulse = True
+        pulse_interval = cli_args.pulse_interval
+    
     pc10._set_filter_parameters(
         cli_args.channel_ids,
-        cli_args.channel_types
+        cli_args.channel_types,
+        use_pulse
     )
 
     have_time = False
@@ -31,6 +39,16 @@ def run_task(cli_args):
     for packet in file:
         file_pos += packet.packet_length
         bar.update_progress(file_pos)
+
+        if use_pulse:
+            if not pulser:
+                if packet.channel_id == 0 and packet.data_type == 0x01:
+                    pulser = pulse.SetupPacketPulse(packet)
+                    pulser.set_interval(pulse_interval)
+            else:
+                setup_packet = pulser.get_pulse()
+                if not setup_packet is None:
+                    buf.append(setup_packet)
 
         if (not have_time):
             if (packet.parent and packet.parent.last_time is not None):
