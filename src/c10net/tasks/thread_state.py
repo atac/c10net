4bd5b17100242f28846abfc8374d1ef6bb78b90f
Stephen Frees <stephenf93@gmail.com>
2026-03-27 11:46:43 -0500

    Added locking get/set pair to ThreadState progress attribute

--- src/c10net/tasks/thread_state.py
+++ src/c10net/tasks/thread_state.py
@@ -1,10 +1,31 @@
 
-from threading import Event
+from threading import Event, Lock
 
 class ThreadState:
     def __init__(self):
         self.terminate = Event()
         self.finish = Event()
-        self.progress = 0.0
+        self._progress = 0.0
+        self._progress_lock = Lock()
         pass
 
+    def set_progress(self, progress : float):
+        """
+        Set the progress value after aquiring the lock.
+
+        Raises ValueError if progress not in range of 0.0 and 1.0
+        """
+        if progress < 0.0 or progress > 1.0:
+            raise ValueError
+        
+        with self._progress_lock:
+            self._progress = progress
+        
+    def get_progress(self):
+        """
+        Get the progress after aquiring the Lock.
+
+        Returns progress as a float between 0.0 and 1.0
+        """
+        with self._progress_lock:
+            return self._progress
