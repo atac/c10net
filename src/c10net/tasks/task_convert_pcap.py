7412060b433655b81aca6b7ad4f4cc761d9d9a8f
Stephen Frees <stephenf93@gmail.com>
2026-03-26 22:06:37 -0500

    Added exceptions and cleaned up cli_args references

--- src/c10net/tasks/task_convert_pcap.py
+++ src/c10net/tasks/task_convert_pcap.py
@@ -1,28 +1,25 @@
-from threading import Thread
 from pathlib import Path
 
 from c10net.tasks.task import Task
 from c10net.tasks.worker import WorkerConfigError
-import c10net.chapter10_to_pcap as c2pcap
 
 from c10net.tasks.parse_chapter10 import ParseChapter10
-from c10net.tasks.chapter10_to_ethernet import C10ToEthernet
+from c10net.tasks.chapter10_to_ethernet import Chapter10ToEthernet
 from c10net.tasks.write_to_pcap import WritePcap
-
-# from c10net.tasks import parse_chapter10 as parse_ch10
-# from c10net.tasks import chapter10_to_ethernet as ch10_to_eth
-# from c10net.tasks import write_to_pcap as write_to_pcap
 
 class ConvertPcap(Task):
     def __init__(self, cli_args : dict):
         super().__init__(cli_args)
 
-        self._init()
+        try:
+            self._init()
 
-        if self._cli_args.parallel:
-            self._setup_parallel()
-        else:
-            self._setup_linear()
+            if self._cli_args['parallel']:
+                self._setup_parallel()
+            else:
+                self._setup_linear()
+        except KeyError as err:
+            raise WorkerConfigError("Required parameters not found") from err
 
 
     def start(self):
@@ -31,17 +28,18 @@ class ConvertPcap(Task):
         self._task_func()
 
     def _init(self):
-        if not self._cli_args.outfile:
-            pn = self._derive_out_pathname(self._cli_args.in_pathname)
-            self._cli_args.outfile = pn
+        if ('outfile' not in self._cli_args
+            or self._cli_args['outfile'] == ""):
+            pn = self._derive_outfile(self._cli_args['in_pathname'])
+            self._cli_args['outfile'] = pn
 
     def _setup_linear(self):
-        write = WritePcap(self._cli_args.outfile)
-        convert = C10ToEthernet(self._cli_args, write.direct_input())
+        write = WritePcap(self._cli_args['outfile'])
+        convert = Chapter10ToEthernet(self._cli_args, write.direct_input())
         parse = ParseChapter10(
-            self._cli_args.in_pathname,
-            self._cli_args.channel_ids,
-            self._cli_args.channel_types,
+            self._cli_args['in_pathname'],
+            self._cli_args['channel_ids'],
+            self._cli_args['channel_types'],
             sink=convert.direct_input()
         )
 
@@ -84,7 +82,7 @@ class ConvertPcap(Task):
 
         
 
-    def _derive_out_pathname(self, in_pathname : str):
+    def _derive_outfile(self, in_pathname : str):
         inpath = Path(in_pathname)
         result = inpath.with_suffix('.pcap')
         return str(result)
