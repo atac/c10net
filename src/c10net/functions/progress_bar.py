9315da08fdc6ecde3ebb098d06fae3e568686d04
Stephen Frees <stephenf93@gmail.com>
2026-03-27 11:45:58 -0500

    Test-driven refactor of progress bar

--- src/c10net/functions/progress_bar.py
+++ src/c10net/functions/progress_bar.py
@@ -1,48 +1,74 @@
 
-_start = 0.0
-_end = 0.0
-_progress = 0.0
+class ProgressBar:
+    def __init__(self, start, end):
+        if (start >= end):
+            raise ValueError()
+        
+        self._start = start
+        self._end = end
+        self._time_range = end - start
+        self._progress = 0.0 # always stored in percent
+        self._precision = 1
+
+    def set_progress_relative(self, progress : float):
+        """Set progress with a value within the range of start and end."""
+
+        progress = self._clamp(progress)
+            
+        curr = progress - self._start
+
+        percent = (curr / self._time_range) * 100.0
+        percent = round(percent, self._precision)
+
+        if (percent > self._progress):
+            self._progress = percent
+
+    def set_progress_absolute(self, progress : float):
+        """Set progress with a value within the range of 0.0 and 1.0"""
+        progress = self._clamp_abs(progress)
+
+        percent = progress * 100.0
+
+        if (percent > self._progress):
+            self._progress = percent
+
+    def get_bar(self):
+        return self._generate_progress_bar()
+    
+    def _clamp(self, value : float):
+        if value < self._start:
+            return self._start
+        elif value > self._end:
+            return self._end
+        else:
+            return value
+    
+    def _clamp_abs(self, value : float):
+        if value < 0.0:
+            return 0.0
+        elif value > 1.0:
+            return 1.0
+        else:
+            return value
 
 
-def set_bounds(start, end):
-    global _start, _end
+    def _generate_progress_bar(self):
+        bar = '|'
+        bar_size = 50
 
-    _start = start
-    _end = end
+        fraction = int((self._progress / 100.0) * bar_size)
 
+        for i in range(0, fraction):
+            bar += '='
+        
+        for i in range(0, bar_size-fraction):
+            bar += '-'
 
-def update_progress(current: float):
-    global _progress, _start, _end
+        bar += f'| {self._progress}%   '
 
-    time_range = _end - _start
-    curr = current - _start
+        return bar
 
-    percent = (curr / time_range) * 100.0
-    percent = round(percent, 1)
-
-    if (percent > _progress):
-        _progress = percent
-        _print_progress()
     
 def _print_progress():
     progress_bar = _generate_progress_bar()
     print('\r' + progress_bar, end='')
-
-def _generate_progress_bar():
-    global _progress
-
-    bar = '|'
-
-    bar_size = 50
-
-    fraction = int((_progress / 100.0) * bar_size)
-
-    for i in range(0, fraction):
-        bar += '='
-    
-    for i in range(0, bar_size-fraction):
-        bar += '-'
-
-    bar += f'| {_progress}%   '
-
-    return bar
