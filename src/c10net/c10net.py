45b87670991a541c74e3685efca456553791471e
Stephen Frees <stephenf93@gmail.com>
2026-05-20 11:19:40 -0500

    implement replay staging

--- src/c10net/c10net.py
+++ src/c10net/c10net.py
@@ -12,6 +12,7 @@ from . import cli
 
 from .tasks.worker import WorkerConfigError
 from .tasks.task_convert_pcap import ConvertPcap
+from .tasks.task_replay import Replay
 from .tasks.watchdog import Watchdog
 
 _task = None # staging for Task object to be run
@@ -31,7 +32,6 @@ def cli_entry():
 
     run()
 
-
 def run():
     global _task
 
@@ -47,67 +47,13 @@ def stage_convert_pcap(cli_args):
     except WorkerConfigError as err:
         err.add_note("Error in convert_pcap")
         raise err
-    
 
 
 def stage_replay(cli_args):
-    None
-    # TODO: implement
-    #source_sink = (parse_ch10.retreive_packets, send_udp.deposit_packets)
-    # _threads.append(Thread(target=parse_ch10.parse_file, args=(args)))
-    # _threads.append(Thread(target=pipe_packets, args=source_sink))
-    # _threads.append(Thread(target=send_udp.replay_packets, args=(args)))
+    global _task
 
-    # _terminate_events.append(parse_ch10.terminate_event)
-    # _terminate_events.append(send_udp.terminate_event)
-
-    # _source_thread = _threads[0]
-    #if (cli_args.parallel):
-    if (False):
-        None
-    else:
-        chapter10_to_replay.run_task(cli_args)
-        sys.exit(0)
-
-
-
-
-
-#     wait_for_keypress_with_confirmation()
-
-# def wait_for_keypress_with_confirmation(prompt_key="Esc", confirm_prompt="Are you sure? (y/n): "):
-#     """Wait for a specific user keypress, then prompt for confirmation before returning."""
-#     global should_terminate
-
-#     print(f"Press {prompt_key} to exit...")
-
-#     while not should_terminate.is_set() and not finished():
-#         # Wait for the keypress
-#         key_pressed, key_timeout = timedKey(prompt_key, timeout=1)
-
-#         if (not key_timeout):
-#             # Prompt for confirmation
-#             response = input(confirm_prompt).strip().lower()
-#             if response in ('y', 'yes'):
-#                 should_terminate.set()
-
-# def finished():
-#     """If source thread is done, set finish events. Returns True if finished."""
-#     global _source_thread, should_finish
-
-#     if (not should_finish and _source_thread and not _source_thread.is_alive()):
-#         should_finish = True
-#         for event in _finish_events:
-#             event.set()
-
-#     for thread in _threads:
-#         if thread.is_alive():
-#             return False
-
-#     return True
-
-# def terminate_all_threads():
-#     """Set all termination events to signal threads to exit."""
-#     for event in _terminate_events:
-#         event.set()
-
+    try:
+        _task = Replay(cli_args)
+    except WorkerConfigError as err:
+        err.add_note("Error in convert_pcap")
+        raise err
